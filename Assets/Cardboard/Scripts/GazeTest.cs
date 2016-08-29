8e610782349713903d4aa914905bf3c4befbca87
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-29 16:41:52 -0700

    Add Trigger Handler

---
+++ Assets/Cardboard/Scripts/GazeTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GazeTest : MonoBehaviour {
+
+    private CardboardHead gazeTestHead;
+    public Text gazeText;
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += PullTrigger;
+        gazeTestHead = FindObjectOfType<CardboardHead>();
+	}
+
+    private void PullTrigger ()
+    {
+        print("Trigger pulled");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        gazeText.text = gazeTestHead.Gaze.ToString();
+	}
+}
