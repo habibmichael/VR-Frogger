9e0153d012560f2ef8157b2d913b83c08c1300b6
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-29 18:54:07 -0700

    Add natural jumping effect, disable flying effect

--- Assets/Cardboard/Scripts/Player.cs
+++ Assets/Cardboard/Scripts/Player.cs
@@ -4,28 +4,53 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    private CardboardHead gazeTestHead;
+    private CardboardHead head;
     public Text gazeText;
     private Rigidbody rb;
+
+    public float jumpSpeed;
+    public float jumpAngleInDegrees;
+
+    //prevents flying effect
+    private bool onGround;
+
 	// Use this for initialization
 	void Start () {
         
         //add handler for trigger events
         Cardboard.SDK.OnTrigger += PullTrigger;
 
-        gazeTestHead = FindObjectOfType<CardboardHead>();
+        head = FindObjectOfType<CardboardHead>();
         rb = GetComponent<Rigidbody>();
 	}
+
+    void OnCollisionEnter(Collision collision )
+    {
+        onGround = true;
+    }
+
+    void OnCollisionExit(Collision collision )
+    {
+        onGround = false;
+    }
 
     private void PullTrigger ()
     {
         //Allows character to jump with each trigger pull
-        rb.AddForce(gazeTestHead.Gaze.direction*1000);
-    }
+        //Add natural jumping
+
+        if (onGround)
+        {
+            float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
+            Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+            Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+            rb.velocity = jumpVector * jumpSpeed;
+        }
+            }
 
     // Update is called once per frame
     void Update () {
 
-        gazeText.text = gazeTestHead.Gaze.ToString();
+        gazeText.text = head.Gaze.ToString();
 	}
 }
