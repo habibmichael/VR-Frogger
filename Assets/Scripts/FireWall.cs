4137912c8fe8fddd45d7beb63d7770ae019f55cd
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-31 23:56:45 -0700

    Add Game Over Canvas and Splash Screen

---
+++ Assets/Scripts/FireWall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWall : MonoBehaviour {
+
+    public float creepSpeed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        followPlayer();
+        creepForward();
+	
+	}
+
+    void followPlayer ()
+    {
+        GameObject player = GameObject.Find("Player");
+        Vector3 delta = player.transform.position - transform.position;
+        Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
+        transform.position += projectedDelta;
+    }
+
+    void creepForward ()
+    {
+        transform.position += Vector3.forward * creepSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void loadNextLevel ()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+}
