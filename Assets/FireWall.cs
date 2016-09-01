c4d693c43736b1c557b7037c389e196213ed91d9
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-31 18:48:14 -0700

    Add Fire wall to follow player

---
+++ Assets/FireWall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
