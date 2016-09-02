a1803467fae001eac0d14daa3ee7e48f01ec1295
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-09-01 21:44:19 -0700

    Add lose condition for collision with vehicles

---
+++ Assets/Scripts/LethalCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalCollider : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision )
+    {
+        GameState state = FindObjectOfType<GameState>();
+        Player player = FindObjectOfType<Player>();
+        if (collision.collider == player.GetComponent<CapsuleCollider>())
+        {
+            state.isGameOver = true;
+        }
+
+        
+    }
+}
