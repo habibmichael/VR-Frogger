8b70531191cd1ae4a912fb4abd6fac292df629c3
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-09-01 20:56:08 -0700

    Instantiate vehicles, and set them to move and be destroyed

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private float speed;
+    private float length;
+
+	// Use this for initialization
+	void Start () {
+        float lifeTime = length / speed;
+        Invoke("remove", lifeTime);
+	}
+
+    void remove ()
+    {
+        Destroy(gameObject);
+    }
+
+    public void setPath(float speed,float length)
+    {
+        this.speed = speed;
+        this.length=length; 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public float startOffset=10f;
+    public GameObject[] vehiclePrefabs;
+    public float speed = 5.0f;
+    public float length = 10f;
+
+   
+    
+
+	// Use this for initialization
+	void Start () {
+
+        instantiateVehicle();
+       
+        
+	}
+
+    Vector3 getPositionOffset ()
+    {
+       Vector3 pos =  transform.position;
+        pos += startOffset * Vector3.right;
+
+        return pos;
+    }
+
+    void instantiateVehicle ()
+    {
+        GameObject vehicle = Instantiate(vehiclePrefabs[0]);
+        vehicle.transform.position = getPositionOffset();
+        vehicle.transform.parent = transform;
+
+        Vehicle vehicleObject = vehicle.GetComponent<Vehicle>();
+        vehicleObject.setPath(speed,length);
+
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
